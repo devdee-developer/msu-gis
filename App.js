a48e0c5577a4cbd511335a1df5d6c3379850a3ab
Ekkarach <ekkarach@devaday.co.th>
2021-07-18 16:24:27 +0700

    to added Stack Navigator for Home Screen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './pages/LoginPage/Screen';
 import SplashScreen from './pages/SplashPage/Screen';
-
+import HomeScreen from './pages/HomePage/Screen';
 class App extends Component {
   
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
       <Stack.Navigator initialRouteName="SplashScreen">
         <Stack.Screen options={{headerShown: false}} name="SplashScreen" component={SplashScreen} />
         <Stack.Screen options={{headerShown: false}} name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: false}} name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     );
