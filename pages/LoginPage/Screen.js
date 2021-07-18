868173f570f73dc46a27a5f9e558ff951ad31238
Ekkarach <ekkarach@devaday.co.th>
2021-07-18 15:20:20 +0700

    finished login page

--- pages/LoginPage/Screen.js
+++ pages/LoginPage/Screen.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import { Text,Button  } from 'react-native';
+import { View,Text,TouchableOpacity,Image,ImageBackground, Button,TextInput } from 'react-native';
 import Style from './Style';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import logo from '../../assets/logo.png';
+import image from '../../assets/bg_login.png';
+import user_lock_login_icon from '../../assets/login_user_lock_icon.png';
 class Screen extends Component {
   render() {
     return (
-      <Button
-      title="back to splash screen"
-      onPress={() =>
-        this.props.navigation.navigate('SplashScreen')
-      }
-    />
+      <View style={[Style.container, {flexDirection: "column"}]}>
+        <ImageBackground 
+        source={{ uri: image }} 
+        resizeMode="cover" 
+        style={Style.bgImage}  
+        imageStyle={Style.bgImageStyle}/>
+        
+        <View style={Style.containerLogo}>
+          <Image source={{ uri: logo }} style={Style.imageLogoSplash} />
+        </View>
+        <View style={Style.text2} >
+          <Text style={{fontSize:25,color:"#000072",fontWeight:"bold"}}>ลงชื่อเข้าใช้งาน</Text>
+        </View>
+        <View style={Style.text3} >
+          <Text style={{fontSize:16,fontWeight:"bold"}}>GIS MOBILE APPLICATION</Text>
+        </View>
+        <View style={{flex:3,flexDirection: "row",
+      justifyContent: 'center',
+      alignItems: 'flex-end'}}>
+          <Image source={{uri:user_lock_login_icon}} style={{width:80,height:80}}></Image>
+
+        </View>
+        <View style={{ flex: 13,alignItems:"center"}} >
+          <TextInput style={{width:"80%",height:50,backgroundColor:"white",borderRadius:50,textAlign:"center",marginTop:30,borderColor:"#B6B5FE",borderWidth:1}}  placeholder="ชื่อผู้ใช้งาน" />
+          <TextInput style={{width:"80%",height:50,backgroundColor:"white",borderRadius:50,textAlign:"center",marginTop:30,borderColor:"#B6B5FE",borderWidth:1}} secureTextEntry={true}  placeholder="รหัสผ่าน" />
+        
+          <TouchableOpacity  onPress={() =>this.props.navigation.navigate('HomeScreen') } style={Style.btnSubmitLogin}>
+                <Text style={{ fontSize: 20, color: '#fff' }}>
+                  ตกลง
+                </Text>
+              </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
+
 export default Screen;
--- pages/LoginPage/Style.js
+++ pages/LoginPage/Style.js
@@ -2,13 +2,49 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color:'#f00'
+        flex: 1
       },
-      textApp:{
-          color:"red"
-      }
+    bgImage:{
+      position: "absolute",
+      width:"100%",
+      height:"100%"
+    },
+    bgImageStyle:{
+      resizeMode: "cover",
+      alignSelf: "flex-end"
+    },
+    btnSplashLogin:{
+      width:"80%",
+      height:50,
+      backgroundColor:"#1bc3a2",
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius:50
+    },
+    btnSubmitLogin:{
+      width:"80%",
+      height:50,
+      backgroundColor:"#9596f3",
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius:50,
+      marginTop:30
+      
+    },
+    imageLogoSplash:{
+      width:150,
+      height:150
+    },
+    containerBtnLoginSplash:{
+      flex:1,
+      flexDirection: "row",
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      marginBottom:20
+    },
+    text1:{ flex: 1,justifyContent: 'center',alignItems: 'center' },
+    text2:{ flex: 1,justifyContent: 'center',alignItems: 'center'},
+    text3:{ flex: 1,justifyContent: 'center',alignItems: 'center' },
+    containerLogo:{ flex: 6,justifyContent: 'center',alignItems: 'center' }
+
 });
