31ce26df6a09361a7e39f32568ac56f100f30a41
Ekkarach <ekkarach@devaday.co.th>
2021-07-18 14:22:46 +0700

    removed import module unnecessary

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import AppStyles from './AppStyle';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import ContactScreen from './pages/ContactPage/Screen';
-import HomeScreen from './pages/HomePage/Screen';
 import LoginScreen from './pages/LoginPage/Screen';
-import NewsScreen from './pages/NewsPage/Screen';
 import SplashScreen from './pages/SplashPage/Screen';
 
 class App extends Component {
