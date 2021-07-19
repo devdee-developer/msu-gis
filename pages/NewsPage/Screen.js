f7aaa563c245c1c3c67e446e0ab3e7a03984fa04
prachaya <prachaya.in@gmail.com>
2021-07-19 12:44:36 +0700

    test commit

--- pages/NewsPage/Screen.js
+++ pages/NewsPage/Screen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+
 import Style from './Style';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Text } from 'react-native';
+
 class Screen extends Component {
   render() {
     return (
