9e8dac6197ccb1a981746926a09077d44e7f177c
popjirapat <jirapat486@gmail.com>
2021-07-21 17:32:40 +0700

    หน้า screen contact ยังไม่ต่อ api

--- pages/ContactPage/Screen.js
+++ pages/ContactPage/Screen.js
@@ -1,14 +1,221 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
-import Style from './Style';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ButtonCustomComponent from '../../components/ButtonCustomComponent/Screen';
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import React, { Component } from 'react'
+
+import Style from './Style'
+import arrow_d from '../../assets/arrow_d.png'
+import arrow_right from '../../assets/arrow_right.png'
+import contact_icon_head from '../../assets/contact_icon_head.png'
+import doc_icon from '../../assets/doc_icon.png'
+import flag_icon from '../../assets/flag_icon.png'
+import health_1_icon from '../../assets/health_1_icon.png'
+import older from '../../assets/older.png'
+import user_icon from '../../assets/user_icon.png'
+import visit_icon from '../../assets/doc_icon.png'
+
 class Screen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeSections: [],
+      titleFilter: 'เลือกพื้นที่ดูแล เพื่อเลือกรายชื่อที่ต้องการ',
+      olderQty: '289',
+      FilterModel: {
+        Province: 'จังหวัด มหาสารคาม',
+        District: 'อบต. สันป่าตอง',
+        Moo: 'หมู่ที่ 5',
+      },
+      showFilterContent: true,
+      olderLists: [
+        {
+          avatar: '',
+          name: 'นาย ชัยสิทธิ์ มิตรมงคลกุล',
+          secondLine: 'อายุ 65 ปี, 3 เดือน',
+          thridLine: '99/8 หมู่ 5 ตำบลสันป่าตอง',
+          healthText: 'ติดเตียง',
+          docText: 'กำลังออกประเมิน..',
+          visitText: 'ออกเยี่ยมแล้ว',
+          healthStatus: 1,
+          docStatus: 1,
+          visitStatus: 2,
+        },
+      ],
+    }
+  }
+  state = {
+    activeSections: [],
+  }
+
+  toggleshowFilterContent() {
+    this.setState({
+      showFilterContent: !this.state.showFilterContent,
+    })
+  }
   render() {
     return (
-      <ButtonCustomComponent title={this.props.title}></ButtonCustomComponent>
-    );
+      <ScrollView style={Style.container}>
+        <View style={Style.containerFilterHead}>
+          <Image
+            source={{ uri: contact_icon_head }}
+            style={Style.imageContactIconHead}
+          />
+          <Text style={Style.textHeadFilter}>{this.state.titleFilter}</Text>
+          <TouchableOpacity onPress={() => this.toggleshowFilterContent()}>
+            <Image source={{ uri: arrow_d }} style={Style.arrow_d} />
+          </TouchableOpacity>
+        </View>
+        {this.state.showFilterContent ? (
+          <View style={Style.containerFilterContent}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'flex-start',
+                backgroundColor: '#010979',
+              }}
+            >
+              <TextInput
+                style={Style.inputProvince}
+                value={this.state.FilterModel.Province}
+                disableFullscreenUI={true}
+                editable={false}
+              />
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                }}
+              >
+                <TextInput
+                  style={Style.inputDistrict}
+                  disableFullscreenUI={true}
+                  editable={false}
+                  value={this.state.FilterModel.District}
+                />
+                <TextInput
+                  style={Style.inputMoo}
+                  value={this.state.FilterModel.Moo}
+                  disableFullscreenUI={true}
+                  editable={false}
+                />
+              </View>
+            </View>
+          </View>
+        ) : (
+          <></>
+        )}
+        <View style={Style.containerOlderQty}>
+          <Text style={Style.textOlderQty}>จำนวณผู้สูงอายุ</Text>
+          <Text style={Style.textNumberOlderQty}>{this.state.olderQty} คน</Text>
+          <View style={{ flex: 1 }}></View>
+        </View>
+        <View style={Style.containerBgOlderList}>
+          <View style={Style.containerOlderList}>
+            <View style={Style.containerImageOlder}>
+              <Image source={{ uri: older }} style={Style.imageOlder} />
+            </View>
+            <View style={Style.containerOlderDetail}>
+              <Text style={Style.textOlderName}>นาย ชัยสิทธิ์ มิตรมงคลกุล</Text>
+              <View style={Style.containerOlderDetailLine2}>
+                <Image
+                  source={{ uri: user_icon }}
+                  style={Style.imageUserIcon}
+                />
+                <Text style={Style.textOlderAge}>อายุ 65 ปี, 3 เดือน</Text>
+              </View>
+
+              <View style={Style.containerOlderDetailLine3}>
+                <Image
+                  source={{ uri: flag_icon }}
+                  style={Style.imageFlagIcon}
+                />
+                <Text style={Style.textOlderAddress}>
+                  99/8 หมู่ 5 ตำบลสันป่าตอง
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Image source={{ uri: arrow_right }} style={Style.arrow_right} />
+            </View>
+          </View>
+          <View style={Style.containerTagHr}>
+            <View style={{ flex: 1, height: 1, backgroundColor: '#E4E1F0' }} />
+          </View>
+          <View style={Style.containerOlderStatus}>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              <View
+                style={{
+                  flex: 10,
+                  backgroundColor: '#010979',
+                  margin: 10,
+                  borderRadius: 50,
+                  flexDirection: 'row',
+                }}
+              >
+                <View style={{ flex: 1, marginLeft: 7 }}>
+                  <Image
+                    source={{ uri: health_1_icon }}
+                    style={Style.imageHealthIcon_1}
+                  />
+                </View>
+                <View style={{ flex: 5, marginLeft: 5 }}>
+                  <Text style={Style.textOlderhealth}>ติดเตียง</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flex: 15,
+                  backgroundColor: '#FCDDE1',
+                  margin: 10,
+                  borderRadius: 50,
+                  flexDirection: 'row',
+                }}
+              >
+                <View style={{ flex: 1, marginLeft: 7 }}>
+                  <Image
+                    source={{ uri: doc_icon }}
+                    style={Style.imageDocIcon}
+                  />
+                </View>
+                <View style={{ flex: 5, marginLeft: 5 }}>
+                  <Text style={Style.textOlderdoc}>กำลังประเมิน...</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flex: 15,
+                  backgroundColor: '#F296A3',
+                  margin: 10,
+                  borderRadius: 50,
+                  flexDirection: 'row',
+                }}
+              >
+                <View style={{ flex: 1, marginLeft: 7 }}>
+                  <Image
+                    source={{ uri: visit_icon }}
+                    style={Style.imageVisitIcon}
+                  />
+                </View>
+                <View style={{ flex: 5, marginLeft: 5 }}>
+                  <Text style={Style.textOldervisit}>รอออกเยี่ยม...</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    )
   }
 }
-export default Screen;
+export default Screen
--- pages/ContactPage/Style.js
+++ pages/ContactPage/Style.js
@@ -1,14 +1,241 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color:'#f00'
-      },
-      textApp:{
-          color:"blue"
-      }
-});
+  // container
+  container: {
+    flex: 1,
+    backgroundColor: '#F7F5FC',
+    flexDirection: 'column',
+  },
+  containerFilterHead: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flex: 1,
+    backgroundColor: '#010979',
+    paddingVertical: 10,
+    flexDirection: 'row',
+  },
+  containerFilterContent: {
+    alignItems: 'center',
+    flex: 3,
+    backgroundColor: '#010979',
+    flexDirection: 'row',
+  },
+  containerOlderQty: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    marginVertical: 10,
+    flexDirection: 'row',
+  },
+  containerOlderList: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  containerBgOlderList: {
+    backgroundColor: '#FFFFFF',
+  },
+  containerImageOlder: {
+    flex: 2,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  containerOlderDetail: {
+    flex: 6,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  containerOlderDetailLine2: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  containerOlderDetailLine3: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  containerTagHr: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  containerOlderStatus: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  // text
+  textApp: {
+    color: 'blue',
+  },
+  textHeadFilter: {
+    fontFamily: 'Prompt',
+    color: '#FFFFFF',
+    fontStyle: 'normal',
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textOlderQty: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#010979',
+    fontStyle: 'normal',
+    fontSize: 18,
+    marginLeft: 10,
+    marginVertical: 20,
+  },
+  textNumberOlderQty: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#6F63FD',
+    fontStyle: 'normal',
+    fontSize: 18,
+    marginVertical: 20,
+  },
+  textOlderName: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#010979',
+    fontStyle: 'normal',
+    fontSize: 22,
+    marginVertical: 4,
+    paddingLeft: 7,
+  },
+  textOlderAge: {
+    flex: 10,
+    fontFamily: 'Prompt',
+    color: '#010979',
+    fontStyle: 'normal',
+    fontSize: 16,
+    marginVertical: 4,
+  },
+  textOlderAddress: {
+    flex: 10,
+    fontFamily: 'Prompt',
+    color: '#3B3D48',
+    fontStyle: 'normal',
+    fontSize: 14,
+    marginVertical: 4,
+  },
+  textOlderhealth: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#FCDDE1',
+    fontStyle: 'normal',
+    fontSize: 14,
+    marginVertical: 4,
+  },
+  textOlderdoc: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#0D0E12',
+    fontStyle: 'normal',
+    fontSize: 14,
+    marginVertical: 4,
+  },
+  textOldervisit: {
+    flex: 1,
+    fontFamily: 'Prompt',
+    color: '#0D0E12',
+    fontStyle: 'normal',
+    fontSize: 14,
+    marginVertical: 4,
+  },
+
+  // image
+  imageContactIconHead: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#010979',
+    resizeMode: 'center',
+    marginHorizontal: 10,
+  },
+  imageLogoSplash: {
+    width: 150,
+    height: 150,
+  },
+  imageOlder: {
+    width: 66,
+    height: 66,
+    resizeMode: 'center',
+    margin: 10,
+  },
+  imageUserIcon: {
+    flex: 1,
+    height: 15,
+    resizeMode: 'center',
+    margin: 6,
+  },
+  imageFlagIcon: {
+    flex: 1,
+    height: 15,
+    resizeMode: 'center',
+    margin: 6,
+  },
+  imageHealthIcon_1: {
+    flex: 1,
+    height: 19,
+    resizeMode: 'contain',
+  },
+  imageDocIcon: {
+    flex: 1,
+    height: 15,
+    resizeMode: 'contain',
+  },
+  imageVisitIcon: {
+    flex: 1,
+    height: 15,
+    resizeMode: 'contain',
+  },
+
+  //icon
+  arrow_d: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#010979',
+    resizeMode: 'center',
+    marginHorizontal: 10,
+  },
+  arrow_right: {
+    width: 10,
+    height: 10,
+    resizeMode: 'center',
+    margin: 10,
+  },
+
+  //input
+  inputProvince: {
+    height: 40,
+    margin: 12,
+    fontSize: 18,
+    borderWidth: 1,
+    backgroundColor: '#DCCFFE',
+    color: '#010979',
+    borderRadius: 10,
+    paddingLeft: 12,
+  },
+  inputDistrict: {
+    height: 40,
+    width: 163,
+    fontSize: 18,
+    margin: 12,
+    borderWidth: 1,
+    backgroundColor: '#DCCFFE',
+    color: '#010979',
+    borderRadius: 10,
+    paddingLeft: 12,
+  },
+  inputMoo: {
+    height: 40,
+    width: 163,
+    fontSize: 18,
+    margin: 12,
+    borderWidth: 1,
+    backgroundColor: '#DCCFFE',
+    color: '#010979',
+    borderRadius: 10,
+    paddingLeft: 12,
+  },
+})
